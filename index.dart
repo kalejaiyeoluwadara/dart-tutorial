c893ed6c07a89e8d6a5a1af00a8481c584f47ebb
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-10-29 14:51:03 +0100

    enums

--- index.dart
+++ index.dart
@@ -1,9 +1,23 @@
 void main(){
-    List<Student> students = [Student("Dara")];
-    final student = students[0];
-    print(student.name);
-}
+    // List
+    //  Maps
+    Map<String,int> marksUserA = {
+        'Math':10,
+        'English':15,
+        'Cs':30
+    };
+    List<Map<String,int>> marks = [{'Math':10,'English':15,'Cs':30},
+    {'Math': 10, 'English': 15, 'Cs': 30},
+    {'Math': 10, 'English': 15, 'Cs': 30},
+  ];
+   marks.map((e){
+    e.forEach((key,val){
+        print('$key:$val');
+    }); 
+   }).toList();
 
+}
+ 
 class Student{
     final String name;
     Student(this.name);
